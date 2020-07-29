4543faa0338a253d8091661a6ad1b8637978fb60
Miguel Ángel Fernández Carrillo <migferna@student.42madrid.com>
2020-07-29 10:04:58 +0200

    Completed

---
+++ TrainMaster/ViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ViewController.swift
+//  TrainMaster
+//
+//  Created by Miguel Ángel Fernández Carrillo on 28/07/2020.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARImageTrackingConfiguration()
+        
+        guard let arImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else { return }
+        
+        configuration.trackingImages = arImages
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard anchor is ARImageAnchor else { return }
+        
+        guard let container = sceneView.scene.rootNode.childNode(withName: "container", recursively: false) else { return }
+        container.removeFromParentNode()
+        node.addChildNode(container)
+        container.isHidden = false
+        
+        let imageScene = SKScene(size: CGSize(width: 720, height: 1280))
+        let imageNode = SKSpriteNode(imageNamed: "plane")
+        imageNode.position = CGPoint(x: imageScene.size.width / 2, y: imageScene.size.height / 2)
+        imageNode.size = imageScene.size
+        imageNode.yScale = -1
+        
+        imageScene.addChild(imageNode)
+        
+        guard let image = container.childNode(withName: "plane", recursively: false) else { return }
+        image.geometry?.firstMaterial?.diffuse.contents = imageScene
+    
+        image.runAction(SCNAction.sequence([SCNAction.wait(duration: 1.0), SCNAction.scale(to: 0.25, duration: 0.5)]))
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
